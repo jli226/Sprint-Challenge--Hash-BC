320c2a611183ac78b5727456afd0e9a9b0887756
Jenny Li <jli226@fordham.edu>
2020-02-14 09:55:18 -0800

    finished hashtables ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,16 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
-    pass
+    # Insert tickets into hash table
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    # Check the hash table for the initial destination and get the first destination
+    ticket = hash_table_retrieve(hashtable, 'NONE')
+
+    # Insert into the route list and grab the next ticket
+    for i in range(length):
+        route[i] = ticket
+        ticket = hash_table_retrieve(hashtable, ticket)
+
+    return route
